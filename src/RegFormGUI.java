d5a3cbc02256c5282a46aa87ac65b74c77bb37c7
Katsiaryna Daron <katya_daron@hotmail.com>
2023-02-02 22:39:53 -0500

    Connecting to database and registering new users

--- src/RegFormGUI.java
+++ src/RegFormGUI.java
@@ -2,11 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class RegFormGUI extends JDialog {
     private JTextField tfName;
     private JTextField tfEmail;
-    private JTextField tfLabel;
+    private JTextField tfPhone;
     private JTextField tfAddress;
     private JPasswordField pfPassword;
     private JPasswordField pfConfirmPassword;
@@ -14,11 +18,13 @@ public class RegFormGUI extends JDialog {
     private JButton btnCancel;
     private JPanel registerPanel;
 
-    public RegFormGUI() {
-        super();
+    public RegFormGUI(JFrame parent) {
+        super(parent);
         setTitle("Create a new account");
         setContentPane(registerPanel);
         setMinimumSize(new Dimension(500, 474));
+        setModal(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -35,9 +41,94 @@ public class RegFormGUI extends JDialog {
     }
 
     private void registerUser() {
+        //reading all the fields
+        String name = tfName.getText();
+        String email = tfEmail.getText();
+        String phone = tfPhone.getText();
+        String address = tfAddress.getText();
+        String password = String.valueOf(pfPassword.getPassword());
+        String confirmPassword = String.valueOf(pfConfirmPassword.getPassword());
+
+        //if any field is empty - show error message
+        if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(this,
+                    "Please enter all fields",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        //checking is the password and the confirmed password are equal
+        if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(this,
+                    "Confirm Password does not match",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        //if there's no error - creating a new user
+        user = addUserToDatabase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        }
+        else {
+            JOptionPane.showMessageDialog(this,
+                    "Failed to register new user",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public User user;
+    private User addUserToDatabase(String name, String email, String phone, String address, String password) {
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/user";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try{
+            //Connect to database
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO registered (name, email, phone, address, password) " +
+                    "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            //Insert row into the table
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public static void main(String[] args) {
-        RegFormGUI myForm = new RegFormGUI();
+        RegFormGUI myForm = new RegFormGUI(null);
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of: " + user.name);
+        }
+        else {
+            System.out.println("Registration canceled");
+        }
     }
 }
